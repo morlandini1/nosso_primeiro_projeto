f8635b8fa120280c459fba24c27f1b8f3e9b8a8c
Marcio Orladini <morlandini1@gmail.com>
2022-06-10 11:19:52 -0300

    Stat

--- lib/main.dart
+++ lib/main.dart
@@ -18,34 +18,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.blue,
-                height: 140,
-              ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Text('Aprender Flutter'),
-                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+        body: Task(),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
 }
+
+
+class Task extends StatelessWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text('Aprender Flutter'),
+                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
