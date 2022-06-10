68aea1a0bb2735424c9369bbeb09d41db863049f
Marcio Orladini <morlandini1@gmail.com>
2022-06-10 12:28:34 -0300

    Stat

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,18 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Task(),
+        body: ListView(
+
+          children: [
+            Task('Aprender Flutter'),
+            Task('Andar de Bike'),
+            Task('Meditar'),
+             Task('Meditar'),
+             Task('Meditar'),
+             Task('Meditar'),
+             Task('Meditar'),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
@@ -27,34 +38,38 @@ class MyApp extends StatelessWidget {
 
 
 class Task extends StatelessWidget {
-  const Task({Key? key}) : super(key: key);
+  final String nome;
+  const Task(this.nome,{Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.black26,
-                  width: 72,
-                  height: 100,
-                ),
-                Text('Aprender Flutter'),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
             ),
-          )
-        ],
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Text(nome),
+                  ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
