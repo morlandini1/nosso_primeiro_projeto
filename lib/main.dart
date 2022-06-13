0a7e405115443b14c40082ad8ed06e273f711354
Marcio Orladini <morlandini1@gmail.com>
2022-06-13 11:17:47 -0300

    LinearProgress

--- lib/main.dart
+++ lib/main.dart
@@ -79,14 +79,25 @@ class _TaskState extends State<Task> {
                             style: TextStyle(
                                 fontSize: 24, overflow: TextOverflow.ellipsis),
                           )),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-                            print(nivel);
-                          },
-                          child: Icon(Icons.arrow_drop_up))
+                      Container(
+                        height: 52,
+                          width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text('Up', style: TextStyle(fontSize: 12),)
+                              ],
+                            )),
+                      )
                     ],
                   ),
                 ),
