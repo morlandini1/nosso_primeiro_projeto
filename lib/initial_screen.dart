80ad6572ecf4cdee94b3be3c4508c32ad1a922e5
Marcio Orladini <morlandini1@gmail.com>
2022-06-14 13:43:20 -0300

    refatoração

---
+++ lib/initial_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/task.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Text('Tarefas'),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1 : 0,
+        duration: Duration(milliseconds: 800),
+        child: ListView(
+          children: [
+            Task(
+                'Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3),
+            Task('Andar de Bike', '', 2),
+            Task('Meditar', '', 5),
+            Task('Ler', '', 4),
+            Task('Jogar', '', 1),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
+        child: Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
